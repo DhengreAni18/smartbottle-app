925104c99b08ef3f0d84d2ba43fc4509128676a5
DhengreAni18 <dhengreani18@gmail.com>
2020-01-17 03:45:26 +0530

    Added Scrollable rows

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-    TouchableHighlight,
-    Image,
-    Alert
-  } from "react-native";
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert,
+  ScrollView
+} from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 class ProfileScreen extends Component {
-    static navigationOptions = {
-      title: "Dashboard"
-    };
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Profile Screen</Text>
-        </View>
-      );
-    }
+  static navigationOptions = {
+    title: "Dashboard"
+  };
+  render() {
+    return (
+      <ScrollView>
+        <Grid>
+          <Row style={{height:200}}>
+            <Text>Fixed width</Text>
+          </Row>
+          <Row style={{height:200}}>
+            <View style={{ flex: 1, backgroundColor: "#6ED4C8" }}></View>
+          </Row>
+          <Row style={{height:200}}>
+            <Text>Fixed width</Text>
+          </Row>
+          <Row style={{height:200}}>
+            <View style={{ flex: 1, backgroundColor: "#6ED4C8" }}></View>
+          </Row>
+        </Grid>
+      </ScrollView>
+    );
   }
+}
 
 export default ProfileScreen;
